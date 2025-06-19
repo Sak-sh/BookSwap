18bd5564091d046be7d335f39cb0a461c7335f2a
SakshiChavan_2005 <96377401+Sak-sh@users.noreply.github.com>
2025-06-19 22:23:02 +0530

    Update RequestList.jsx

--- frontend/src/Components/RequestList.jsx
+++ frontend/src/Components/RequestList.jsx
@@ -17,7 +17,7 @@ const RequestsList = () => {
     setError("");
     try {
       const token = localStorage.getItem("token");
-      const res = await axios.get("http://localhost:5000/api/swap/incoming", {
+      const res = await axios.get("https://bookswap-mi28.onrender.com/api/swap/incoming", {
         headers: { Authorization: `Bearer ${token}` },
       });
       setRequests(res.data);
