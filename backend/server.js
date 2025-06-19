0c3b0ffa3183b0c91597ca75a5a451669f140719
SakshiChavan_2005 <96377401+Sak-sh@users.noreply.github.com>
2025-06-19 22:49:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ setupSocket(io);
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI  {
+mongoose.connect(process.env.MONGO_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
