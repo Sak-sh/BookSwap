bbb19ac7f7cba64f10c6268df7e4b97bfe9ae3a7
SakshiChavan_2005 <96377401+Sak-sh@users.noreply.github.com>
2025-06-19 23:12:29 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,9 +1,9 @@
-// src/utils/socket.js
 import { io } from "socket.io-client";
 
 const socket = io("https://bookswap-mi28.onrender.com", {
-  transports: ['websocket'],  
-  secure: true,               
-  withCredentials: true       
+  transports: ['websocket'],
+  secure: true,
+  withCredentials: true
 });
+
 export default socket;
