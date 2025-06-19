b326d8eaf1e55054f74c408ae0e503548b36973e
SakshiChavan_2005 <96377401+Sak-sh@users.noreply.github.com>
2025-06-19 22:20:28 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -19,7 +19,7 @@ const Register = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://bookswap-mi28.onrender.com/api/auth/register", {
         email,
         password,
       });
